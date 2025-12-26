1aa523b912b2c1aa3020f34e7a6521ad0784512e
Marcel017 <mcelogabriel17@gmail.com>
2025-12-26 18:12:28 -0300

    exercicio-mod-8

---
+++ exercicios/TarefaCalculoMedia/src/CalculadoraMedia.java
@@ -0,0 +1,12 @@
+public class CalculadoraMedia {
+    public static void main(String[] args) {
+        double nota1 = 9.9;
+        double nota2 = 8.6;
+        double nota3 = 9.0;
+        double nota4 = 4.5;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A média final é " + media);
+    }
+}
